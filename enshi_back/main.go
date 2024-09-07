c1fa7085757fd34cbdefff05c926653232f16781
Max <er4gonik@gmail.com>
2024-09-07 21:27:57 +0300

    adad

--- enshi_back/main.go
+++ enshi_back/main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func lookupEnv(dest *string, envVar string) error {
+	if v, exists := os.LookupEnv(envVar); !exists {
+		return fmt.Errorf("%v not found in local env", envVar)
+	} else {
+		*dest = v
+		return nil
+	}
+}
 
+func main() {
+	var bd_pass, bd_user string
 	var err error
 
 	if err = godotenv.Load("secret.env"); err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
-	var test1 string
-	var exists bool
-	if test1, exists = os.LookupEnv("TEST1"); !exists {
+
+	if err := lookupEnv(&bd_pass, "BD_PASSWORD"); err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+	if err := lookupEnv(&bd_user, "BD_USER"); err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
 
-	fmt.Printf("Hey!, %v", test1)
+	Dbx, err := pgxpool.New(context.Background(), fmt.Sprintf("postgres://%v:%v@nekiiinkognito.ru:5432/enshi_db", bd_user, bd_pass))
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
+	}
+	print(Dbx)
+
+	fmt.Printf("Hey!, %v", "you")
 }
